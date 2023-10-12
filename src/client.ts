c3a23f1184bdb03e9bc127d26acfefca7b84e167
AlexDanilovich <alexeidanilovich031193@gmail.com>
2023-10-12 23:36:28 +0200

    feat: added connectDevExtension function for testing

--- src/client.ts
+++ src/client.ts
@@ -3,6 +3,7 @@ import { Manager } from '@lomray/react-mobx-manager';
 import MobxLocalStorage from '@lomray/react-mobx-manager/storages/local-storage';
 import entryClient from '@lomray/vite-ssr-boost/browser/entry';
 import getServerState from '@lomray/vite-ssr-boost/helpers/get-server-state';
+import { spy } from 'mobx';
 import { IS_PROD } from '@constants/index';
 import StateKey from '@constants/state-key';
 import routes from '@routes/index';
@@ -16,6 +17,30 @@ const storeManager = new Manager({
   initState,
   storage: new MobxLocalStorage(),
 });
+
+const connectDevExtension = (manager: Manager) => {
+  // if (!manager['__devOnChange']) {
+  //   return;
+  // }
+
+  spy((event) => {
+    if (event.observableKind === 'object') {
+      // console.log('event:', event);
+      // console.log(`new event ${event.type}:`, {
+      //   ...event,
+      //   object: { ...event.object, suspense: undefined },
+      // });
+
+      manager?.['__devOnChange']?.({
+        event: { ...event, object: { ...event.object, suspense: undefined } },
+      });
+    }
+  });
+
+  return manager;
+};
+
+window['__MOBX_STORE_MANAGER__'] = connectDevExtension(storeManager);
 
 /**
  * Configure client
